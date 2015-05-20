20167444a04aa809d699f1c70a87b8924a12db6f
Li Xu <li@iamxu.me>
2015-05-20 01:13:09 -0700

    set up gulp, sass, and autoprefixer

---
+++ README.md
@@ -0,0 +1,46 @@
+# gulp scaffold
+
+Construct gulp playground step by step
+
+## features
+
+* opinionated browserify to as CommonJS loader
+* CSS optimize
+* browser-sync to livereload
+* map folder structure from src to bld/dist
+
+## TODO
+
+* source map
+* Babel to support ES6 and JSX
+* deb and prod mode
+
+## Usage
+
+__Clone this repository:__
+
+`git clone git@https://github.com/lix42/gulp-scaffol`
+
+__Install the dependencies:__
+
+`npm install`
+
+__Development mode with livereload:__
+
+`gulp watch`
+
+__When you are done, create a production ready version of the JS bundle:__
+
+`gulp build`
+
+## Tools used:
+- Gulp
+- gulp-sass
+- gulp-autoprefixer
+- browserify
+- watchify
+- vinyl-transform
+- jshint
+- lazypipe
+- browser-sync
+
---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+/* jshint globalstrict:true */
+/* global require, console */
+"use strict";
+
+var _ = require("lodash"),
+  gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  minifycss = require("gulp-minify-css"),
+  rename = require("gulp-rename"),
+  sourcemaps = require("gulp-sourcemaps"),
+  del = require("del"),
+  lazypipe = require("lazypipe"),
+  browserSync = require("browser-sync"),
+  reload = browserSync.reload;
+
+var srcScss = "./styles.scss",
+  srcCss = "./NavigationBar.css",
+  srcStatic = "./*.+(html|jpg|png|gif|svg)",
+  outputCss = "./styles",
+  cssPipe;
+
+gulp.task("browser-sync", function() {
+  browserSync({
+    server: {
+      baseDir: ['./']
+    },
+    ui: {
+      port: 8000
+    },
+    files: [
+      srcStatic
+    ],
+    watchOptions: {
+      debounceDelay: 500
+    },
+    open: false
+  });
+});
+
+cssPipe = lazypipe()
+    .pipe(autoprefixer, "{ browsers: ['last 2 version', 'ie 9', '> 5%'], onError: function (err) { console.log(err)} }")
+    .pipe(gulp.dest, outputCss)
+    .pipe(rename, {
+              suffix: ".min"
+          })
+    .pipe(minifycss)
+    .pipe(gulp.dest, outputCss)
+    .pipe(reload, {
+              stream: true
+          });
+
+gulp.task("css", function () {
+    return gulp.src(srcCss)
+        .pipe(cssPipe());
+});
+
+gulp.task("scss", function () {
+    return gulp.src(srcScss)
+        .pipe(sass())
+        .pipe(cssPipe());
+});
+
+gulp.task("clean", function(cb) {
+  del(outputCss, cb);
+});
+
+gulp.task("build", ["clean"], function() {
+  gulp.start("css", "scss");
+});
+
+gulp.task("watch", ["browser-sync"], function() {
+  gulp.watch(srcCss, ["css"]);
+  gulp.watch(srcScss, ["scss"]);
+  gulp.watch(srcStatic, [reload]);
+});
+
+gulp.task('default', function() {
+  console.log('Run "gulp watch or gulp build"');
+});
